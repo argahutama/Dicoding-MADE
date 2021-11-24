1ef23a2cdb2803779f4bbbb3e57ff9ebf93f7137
argahutama <argahutama>
2021-11-24 23:19:17 +0700

    setup main activity

--- app/src/main/java/com/argahutama/submission/made/main/MainActivity.kt
+++ app/src/main/java/com/argahutama/submission/made/main/MainActivity.kt
@@ -1,14 +1,77 @@
 package com.argahutama.submission.made.main
 
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 import com.argahutama.submission.core.base.BaseActivity
+import com.argahutama.submission.custom_ui.CustomSnack
+import com.argahutama.submission.made.R
 import com.argahutama.submission.made.databinding.ActivityMainBinding
+import com.argahutama.submission.made.movie.MovieFragment
+import com.argahutama.submission.made.tvshow.TvShowFragment
 import org.koin.android.viewmodel.ext.android.viewModel
 
 class MainActivity : BaseActivity() {
+    var visibleMenuId: Int? = null
     override val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     override val viewModel by viewModel<MainViewModel>()
 
-    override fun initView() {}
+    override fun setup() {
+        super.setup()
+        initInitialMenu()
+    }
 
-    override fun initAction() {}
+    override fun initView() = with(binding) {
+        bnvMainMenu.itemIconTintList = null
+    }
+
+    override fun initAction() = with(binding) {
+        bnvMainMenu.setOnItemSelectedListener(null)
+        bnvMainMenu.setOnItemSelectedListener { selectMenu(it.itemId) }
+    }
+
+    private fun changeNavigation(fragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.frameContainer, fragment)
+            .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+            .commit()
+    }
+
+    private fun initInitialMenu() = with(binding) {
+        val menuId = visibleMenuId
+        bnvMainMenu.selectedItemId = menuId ?: 0
+    }
+
+    private val className: String get() = "com.argahutama.submission.favorite.FavoriteFragment"
+
+    private fun moveToFavoriteFragment() {
+        val fragment = instantiateFragment(className)
+        if (fragment != null) changeNavigation(fragment)
+    }
+
+    private fun instantiateFragment(className: String): Fragment? = try {
+        Class.forName(className).newInstance() as Fragment
+    } catch (e: Exception) {
+        showSnackbar(e.cause?.message.orEmpty(), CustomSnack.FAILED)
+        null
+    }
+
+    private fun selectMenu(menuId: Int): Boolean {
+        visibleMenuId = menuId
+        return when (menuId) {
+            R.id.main_menu_movies -> {
+                changeNavigation(MovieFragment())
+                true
+            }
+            R.id.main_menu_tv_shows -> {
+                changeNavigation(TvShowFragment())
+                true
+            }
+            R.id.main_menu_favorites -> {
+                moveToFavoriteFragment()
+                true
+            }
+            else -> false
+        }
+    }
 }
