6790b502e30b1adc82d97a6e10689c49fb4a8ebf
argahutama <argahutama>
2021-11-24 21:14:53 +0700

    setup FavoriteViewModel

--- favorite/src/main/java/com/argahutama/submission/favorite/FavoriteViewModel.kt
+++ favorite/src/main/java/com/argahutama/submission/favorite/FavoriteViewModel.kt
@@ -1,6 +1,18 @@
 package com.argahutama.submission.favorite
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.asLiveData
 import com.argahutama.submission.core.base.BaseViewModel
+import com.argahutama.submission.core.domain.model.Movie
+import com.argahutama.submission.core.domain.usecase.MovieUseCase
 
-class FavoriteViewModel: BaseViewModel() {
+class FavoriteViewModel(private val movieUseCase: MovieUseCase) : BaseViewModel() {
+    fun getFavoriteMovies(sort: String): LiveData<List<Movie>> =
+        movieUseCase.getFavoriteMovies(sort).asLiveData()
+
+    fun getFavoriteTvShows(sort: String): LiveData<List<Movie>> =
+        movieUseCase.getFavoriteTvShows(sort).asLiveData()
+
+    fun setFavorite(Movie: Movie, newState: Boolean) =
+        movieUseCase.setMovieFavorite(Movie, newState)
 }
--- favorite/src/main/java/com/argahutama/submission/favorite/di/FavoriteModule.kt
+++ favorite/src/main/java/com/argahutama/submission/favorite/di/FavoriteModule.kt
@@ -1,5 +1,6 @@
 package com.argahutama.submission.favorite.di
 
+import com.argahutama.submission.favorite.FavoriteViewModel
 import org.koin.android.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
