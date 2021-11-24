f2ba7887a1875ea306c5236c5bafd123e23067d5
argahutama <argahutama>
2021-11-24 22:30:29 +0700

    setup splash

--- app/src/main/java/com/argahutama/submission/made/splash/SplashActivity.kt
+++ app/src/main/java/com/argahutama/submission/made/splash/SplashActivity.kt
@@ -1,15 +1,26 @@
 package com.argahutama.submission.made.splash
 
 import android.annotation.SuppressLint
+import androidx.lifecycle.lifecycleScope
 import com.argahutama.submission.core.base.BaseActivity
+import com.argahutama.submission.core.navigation.NavigationDirection
 import com.argahutama.submission.made.databinding.ActivitySplashBinding
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import org.koin.android.viewmodel.ext.android.viewModel
 
 @SuppressLint("CustomSplashScreen")
 class SplashActivity : BaseActivity() {
+    private val delayInMillis = 1500L
+    override val fullscreen = true
     override val binding by lazy { ActivitySplashBinding.inflate(layoutInflater) }
     override val viewModel by viewModel<SplashViewModel>()
     override fun initView() {}
 
-    override fun initAction() {}
+    override fun initAction() {
+        lifecycleScope.launch {
+            delay(delayInMillis)
+            navigateTo(NavigationDirection.Main)
+        }
+    }
 }
