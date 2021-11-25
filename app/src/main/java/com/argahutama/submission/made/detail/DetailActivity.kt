c3b496839b47530112b68775d4376de82007c73f
argahutama <argahutama>
2021-11-25 12:57:59 +0700

    implement detail page

--- app/src/main/java/com/argahutama/submission/made/detail/DetailActivity.kt
+++ app/src/main/java/com/argahutama/submission/made/detail/DetailActivity.kt
@@ -1,9 +1,12 @@
 package com.argahutama.submission.made.detail
 
+import androidx.core.content.ContextCompat
 import com.argahutama.submission.core.base.BaseActivity
 import com.argahutama.submission.core.domain.model.Movie
 import com.argahutama.submission.core.navigation.Extra
+import com.argahutama.submission.made.R
 import com.argahutama.submission.made.databinding.ActivityDetailBinding
+import com.bumptech.glide.Glide
 import org.koin.android.viewmodel.ext.android.viewModel
 
 class DetailActivity : BaseActivity() {
@@ -16,11 +19,50 @@ class DetailActivity : BaseActivity() {
         super.setup()
     }
 
-    override fun initView() {}
+    override fun initView() = with(binding) {
+        ctvTitleDetail.text = movie?.title.orEmpty()
+        ctvDate.text = movie?.releaseDate
+        ctvOverview.text = movie?.overview
+        ctvPopularity.text = getString(
+            R.string.popularity_detail,
+            movie?.popularity.toString(),
+            movie?.voteCount.toString(),
+            movie?.voteAverage.toString()
+        )
+        ctvRating.text = movie?.voteAverage.toString()
+        Glide.with(this@DetailActivity)
+            .load(getString(R.string.base_image_url, movie?.posterPath))
+            .into(ivPosterTopBar)
+        ivPosterTopBar.tag = movie?.posterPath.orEmpty()
 
-    override fun initAction() {}
+        Glide.with(this@DetailActivity)
+            .load(getString(R.string.base_image_url, movie?.posterPath))
+            .into(sivSubPoster)
+        sivSubPoster.tag = movie?.posterPath
+    }
+
+    override fun initAction() = with(binding) {
+        var favoriteState = movie?.favorite ?: false
+        setFavorite(favoriteState)
+        favoriteButton.setOnClickListener {
+            favoriteState = !favoriteState
+            movie?.let { movie -> viewModel.setFavoriteMovie(movie, favoriteState) }
+            setFavorite(favoriteState)
+        }
+        ivBackButton.setOnClickListener { onBackPressed() }
+    }
 
     private fun loadArgs() {
         movie = intent.getParcelableExtra(Extra.MOVIE)
     }
+
+    private fun ActivityDetailBinding.setFavorite(state: Boolean) =
+        if (state)
+            favoriteButton.setImageDrawable(
+                ContextCompat.getDrawable(this@DetailActivity, R.drawable.ic_favorite_selected)
+            )
+        else favoriteButton.setImageDrawable(
+            ContextCompat.getDrawable(this@DetailActivity, R.drawable.ic_favorite_unselected)
+        )
+
 }
--- core/src/main/java/com/argahutama/submission/core/ui/MovieAdapter.kt
+++ core/src/main/java/com/argahutama/submission/core/ui/MovieAdapter.kt
@@ -1,10 +1,12 @@
 package com.argahutama.submission.core.ui
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.argahutama.submission.core.BuildConfig
+import com.argahutama.submission.core.R
 import com.argahutama.submission.core.databinding.ItemMovieBinding
 import com.argahutama.submission.core.domain.model.Movie
 import com.argahutama.submission.core.util.DifferenceUtil
@@ -29,7 +31,7 @@ class MovieAdapter : RecyclerView.Adapter<MovieAdapter.MovieViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
         val bd = ItemMovieBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return MovieViewHolder(bd)
+        return MovieViewHolder(bd, parent.context)
     }
 
     override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
@@ -39,7 +41,8 @@ class MovieAdapter : RecyclerView.Adapter<MovieAdapter.MovieViewHolder>() {
     override fun getItemCount() = listData.size
 
     inner class MovieViewHolder(
-        private val binding: ItemMovieBinding
+        private val binding: ItemMovieBinding,
+        private val context: Context
     ) : RecyclerView.ViewHolder(binding.root) {
         fun bind(movie: Movie) = with(binding) {
             ctvTitle.text = movie.title
@@ -47,7 +50,7 @@ class MovieAdapter : RecyclerView.Adapter<MovieAdapter.MovieViewHolder>() {
             ctvRating.text = movie.voteAverage.toString()
 
             Glide.with(itemView.context)
-                .load("${BuildConfig.IMAGE_URL}${movie.posterPath}")
+                .load(context.getString(R.string.base_image_url, movie.posterPath))
                 .listener(GlideListener(sivMovie, shimmer))
                 .into(sivMovie)
         }
