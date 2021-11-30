4ec277c3feae729808c9ad83bd5f84d441909c77
argahutama <argahut@gmail.com>
2021-12-01 01:19:54 +0700

    add db encryption & ssl pinning

--- core/src/main/java/com/argahutama/submission/core/di/CoreModule.kt
+++ core/src/main/java/com/argahutama/submission/core/di/CoreModule.kt
@@ -9,6 +9,9 @@ import com.argahutama.submission.core.data.source.remote.RemoteDataSource
 import com.argahutama.submission.core.data.source.remote.network.ApiService
 import com.argahutama.submission.core.domain.repository.IMovieRepository
 import com.argahutama.submission.core.util.AppExecutors
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.android.ext.koin.androidContext
@@ -20,19 +23,26 @@ import java.util.concurrent.TimeUnit
 val dbModule = module {
     factory { get<MovieDb>().movieDao() }
     single {
+        val passphrase: ByteArray = SQLiteDatabase.getBytes("argahutama".toCharArray())
+        val factory = SupportFactory(passphrase)
         Room.databaseBuilder(
             androidContext(),
             MovieDb::class.java, "movie.db"
-        ).fallbackToDestructiveMigration().build()
+        ).fallbackToDestructiveMigration().openHelperFactory(factory).build()
     }
 }
 
 val networkModule = module {
     single {
+        val hostname = "api.themoviedb.org"
+        val certificatePinner = CertificatePinner.Builder()
+            .add(hostname, "sha256/oD/WAoRPvbez1Y2dfYfuo4yujAcYHXdv1Ivb2v2MOKk=")
+            .build()
         OkHttpClient.Builder()
             .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
             .connectTimeout(40, TimeUnit.SECONDS)
             .readTimeout(40, TimeUnit.SECONDS)
+            .certificatePinner(certificatePinner)
             .build()
     }
     single {
